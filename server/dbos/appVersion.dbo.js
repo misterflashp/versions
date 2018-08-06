90b28ec8454eb38f51c4226fa9d3c808d85e081d
Tony Stark <ironman0x7b2@protonmail.com>
2018-08-06 14:07:27 +0530

    Added app code.

--- server/dbos/appVersion.dbo.js
+++ server/dbos/appVersion.dbo.js
@@ -1,63 +1,63 @@
 let AppModel = require('../models/appVersion.model');
 
 
-let getAppDetails = (version, type, cb) => {
-    AppModel.findOne({
-        version,
-        type
-    }, {
-            '_id': 0
-        }, (error, result) => {
-            if (error) cb(error, null);
-            cb(null, result || null);
-        });
-};
-
-let addAppDetails = (appDetails, cb) => {
-    let app = new AppModel(appDetails);
-    app.save((error, result) => {
-        if (error) cb(error, null);
-        cb(null, result || null);
+let getAppDetails = (version, appCode, cb) => {
+  AppModel.findOne({
+    version,
+    appCode
+  }, {
+      '_id': 0
+    }, (error, result) => {
+      if (error) cb(error, null);
+      cb(null, result || null);
     });
 };
 
-let listAllVersions = (type, cb) => {
-    AppModel.find({
-        type
-    }, {
-            '_id': 0
-        }, {
-            sort: {
-                'version': -1
-            }
-        }, (error, result) => {
-            console.log(error, result);
-            if (error) cb(error, null);
-            else cb(null, result || []);
-        });
+let addAppDetails = (appDetails, cb) => {
+  let app = new AppModel(appDetails);
+  app.save((error, result) => {
+    if (error) cb(error, null);
+    cb(null, result);
+  });
 };
 
-let getUpdatedVersions = (version, type, cb) => {
-    AppModel.find({
-        type,
-        'version': {
-            $gt: version
-        }
+let listAllVersions = (appCode, cb) => {
+  AppModel.find({
+    appCode
+  }, {
+      '_id': 0
     }, {
-            '_id': 0
-        }, {
-            sort: {
-                'version': -1
-            }
-        }, (error, result) => {
-            if (error) cb(error, null);
-            else cb(null, result || []);
-        });
+      sort: {
+        'version': -1
+      }
+    }, (error, result) => {
+      console.log(error, result);
+      if (error) cb(error, null);
+      else cb(null, result || []);
+    });
+};
+
+let getUpdatedVersions = (version, appCode, cb) => {
+  AppModel.find({
+    appCode,
+    'version': {
+      $gt: version
+    }
+  }, {
+      '_id': 0
+    }, {
+      sort: {
+        'version': -1
+      }
+    }, (error, result) => {
+      if (error) cb(error, null);
+      else cb(null, result || []);
+    });
 };
 
 module.exports = {
-    getAppDetails,
-    addAppDetails,
-    listAllVersions,
-    getUpdatedVersions
+  getAppDetails,
+  addAppDetails,
+  listAllVersions,
+  getUpdatedVersions
 };
